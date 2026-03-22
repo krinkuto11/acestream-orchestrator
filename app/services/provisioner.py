dcac5a6b58db8affa2ae3bb5e02a8be4f7508a45
Ander Regidor <code@anderregidor.es>
2026-03-23 00:06:53 +0100

    feat: Add live buffer option to base command for AceServe variants

--- app/services/provisioner.py
+++ app/services/provisioner.py
@@ -310,17 +310,17 @@ def get_variant_config(variant: str) -> dict:
         "AceServe-amd64": {
             "image": "ghcr.io/krinkuto11/acestream:latest-amd64",
             "config_type": "cmd",
-            "base_cmd": ["python", "main.py", "--bind-all",  "--live-cache-type", "memory", "--live-mem-cache-size", "52428800", "--disable-sentry", "--log-stdout", "--disable-upnp" ],
+            "base_cmd": ["python", "main.py", "--bind-all", "--live-cache-type", "memory", "--live-mem-cache-size", "52428800", "--live-buffer", "25", "--disable-sentry", "--log-stdout", "--disable-upnp"],
         },
         "AceServe-arm32": {
             "image": "ghcr.io/krinkuto11/acestream:base-arm32",
             "config_type": "cmd",
-            "base_cmd": ["python", "main.py", "--bind-all",  "--live-cache-type", "memory", "--live-mem-cache-size", "104857600", "--disable-sentry", "--log-stdout", "--disable-upnp" ],
+            "base_cmd": ["python", "main.py", "--bind-all", "--live-cache-type", "memory", "--live-mem-cache-size", "104857600", "--live-buffer", "25", "--disable-sentry", "--log-stdout", "--disable-upnp"],
         },
         "AceServe-arm64": {
             "image": "ghcr.io/krinkuto11/acestream:base-arm64",
             "config_type": "cmd",
-            "base_cmd": ["python", "main.py", "--bind-all",  "--live-cache-type", "memory", "--live-mem-cache-size", "104857600", "--disable-sentry", "--log-stdout", "--disable-upnp" ],
+            "base_cmd": ["python", "main.py", "--bind-all", "--live-cache-type", "memory", "--live-mem-cache-size", "104857600", "--live-buffer", "25", "--disable-sentry", "--log-stdout", "--disable-upnp"],
         }
     }
     
